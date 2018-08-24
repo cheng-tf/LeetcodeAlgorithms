78f7d6d48c3f4713d08b572dd77edf9986393e3d
zhaoxiyuan <zhaohongzxy@163.com>
2018-08-24 16:17:24 +0800

    提交

--- src/main/java/SortAlgorithms/JianzhiOffer_xx_InversePairsBasedOnMergeSort.java
+++ src/main/java/SortAlgorithms/JianzhiOffer_xx_InversePairsBasedOnMergeSort.java
@@ -1,24 +1,74 @@
 package SortAlgorithms;
 
+import org.junit.Test;
+
 import java.util.Arrays;
 
 public class JianzhiOffer_xx_InversePairsBasedOnMergeSort {
 
+    /******************剑指Offer51:数组中的逆序对**********************************/
+
+    /**
+     * 题目描述
+     * 在数组中的两个数字，如果前面一个数字大于后面的数字，
+     * 则这两个数字组成一个逆序对。输入一个数组,求出这个数组中的逆序对的总数P。
+     * 并将P对1000000007取模的结果输出。 即输出P%1000000007
+     *  示例：输入 1,2,3,4,5,6,7,0  输出 7
+     *
+     *  思路：归并思想。归并思想需要复制一个该数组作为辅助数组，空间复杂度O(N)。
+     *  递归分割，原数组和辅助数组交替交换角色使用，直至为子数组长度为1时，
+     *  逐层返回，返回之前需要统计数目和将两个子数组归并排序。
+     *  递归思想：递归终止条件low==high，返回0；
+     *            首先获取左子数组内部的逆序对数；(递归)
+     *            然后获取右子数组内部的逆序对数；（递归）
+     *            最后将两个子数组合并，并合计所有的逆序对数。（归并）
+     *
+     *  由于数字比较大，在求余之前，利用long存储数组，否则无法通过。
+     */
     public int InversePairs(int [] array) {
         if(array == null||array.length <= 1) return 0;
-        int[] copyArray = Arrays.copyOf(array,array.length);
-        int count = countInversePairs(array,copyArray,0,array.length-1);
-        return 0;
+        int[] copyArray = Arrays.copyOf(array,array.length);//创建辅助数组
+        long count = countInversePairs(array,copyArray,0,array.length-1);
+        return (int)(count%1000000007);
     }
 
-    public int countInversePairs(int[] array,int[] copyArray,int low,int high){
+    /**
+     * 递归方法
+     * 主要是利用了归并思想
+     */
+    public long countInversePairs(int[] array,int[] copyArray,int low,int high){
         if(low == high){//复制这一个即可
             copyArray[low] = array[low];
+            return 0;
         }
         int halfLen = (high - low + 1)>>1;//一般的长度
-        countInversePairs(array,copyArray,low,low+ halfLen -1);
-        countInversePairs(array,copyArray,low+halfLen,high);
-
-        return 0;
+        //递归调用获取左子数组中的逆序对数
+        long leftCount = countInversePairs(copyArray,array,low,low+ halfLen -1);
+        //递归调用获取右子数组中的逆序对数
+        long rightCount = countInversePairs(copyArray,array,low+halfLen,high);
+        //合并两个子数组为一个排序数组
+        long count = leftCount + rightCount;
+        //归并思想
+        int i = low+halfLen-1, j = high, indexCopy = high;
+        while(i >= low &&j >= low+halfLen){
+            if(array[i] > array[j]){
+                copyArray[indexCopy--] = array[i--];
+                count += j-low-halfLen + 1;
+            }else{
+                copyArray[indexCopy--] = array[j--];
+            }
+        }
+        while(i >= low) copyArray[indexCopy--] = array[i--];
+        while(j >= low+halfLen) copyArray[indexCopy--] = array[j--];
+        return count;
     }
+
+    //测试
+    @Test
+    public void test(){
+        int[] nums = {4,5,3,2,6,9,7};
+        int count = InversePairs(nums);
+        System.out.println("count = " + count);
+    }
+
 }
--- src/main/java/SortAlgorithms/MergeSort.java
+++ src/main/java/SortAlgorithms/MergeSort.java
@@ -14,10 +14,10 @@ public class MergeSort {
      * 两种实现方式：1递归方法；非递归方法。
      */
 
-    public void merge(int[] nums,int low,int mid,int high){
+    public void merge2(int[] nums,int low,int mid,int high){
         //默认low--mid和mid+1---high为两个排好的序的子数组
         int len = high-low+1;
-        int[] temp = new int[len];
+        int[] temp = new int[len];//每次merge都要创建一个数组
         int i = low,j = mid+1;
         int index = 0;
         while(i <= mid&&j<=high){
@@ -36,12 +36,12 @@ public class MergeSort {
      * @param low
      * @param high
      */
-    public void mergeSort(int[] nums,int low,int high){
+    public void mergeSort2(int[] nums,int low,int high){
         if(low >= high) return;
         int mid = (low+high)>>1;
-        mergeSort(nums,low,mid);
-        mergeSort(nums,mid+1,high);
-        merge(nums,low,mid,high);
+        mergeSort2(nums,low,mid);
+        mergeSort2(nums,mid+1,high);
+        merge2(nums,low,mid,high);
     }
 
     @Test
@@ -49,14 +49,43 @@ public class MergeSort {
         //测试merge方法
         int[] nums = {1,3,4,5,2,4,6,11};
         System.out.println("Arrays.toString(num) = " + Arrays.toString(nums));
-        merge(nums,0,3, nums.length-1);
+        merge2(nums,0,3, nums.length-1);
         System.out.println("Arrays.toString(num) = " + Arrays.toString(nums));
 
         //递归法：归并排序
-        int[] nums2 = {3,5,1,-10,99,89,32,11,893,-10,0,78,45,34,11,-10,0,23};
+        int[] nums2 = {3,5,1,-10,99,89,32,11,893,-10,0,78,45,34,11,-10,0,23,8};
         System.out.println("Arrays.toString(num2) = " + Arrays.toString(nums2));
-        mergeSort(nums2,0,nums2.length-1);
+        mergeSort2(nums2,0,nums2.length-1);
         System.out.println("Arrays.toString(num2) = " + Arrays.toString(nums2));
+
+        //递归法：归并排序
+        int[] nums3 = {3,5,1,-10,99,89,32,11,893,-10,0,78,45,34,11,-10,0,23,8,-9,0};
+        System.out.println("Arrays.toString(num3) = " + Arrays.toString(nums3));
+        mergeSort(nums3);
+        System.out.println("Arrays.toString(num3) = " + Arrays.toString(nums3));
     }
 
+
+    /***********************归并排序：使用固定的辅助数组**********************************/
+    public void mergeSort(int[] nums){//经过测试，无论nums数组是奇数还是偶数，正确结果都存在nums中，而copyNums差一次
+        int[] copyNums = Arrays.copyOf(nums,nums.length);
+        mergeSort(nums,copyNums,0,nums.length-1);
+    }
+
+    public void mergeSort(int[] nums,int[] copyNums,int low,int high){
+        if(nums == null|| nums.length <= 1||low >= high) return ;
+        int mid = (low + high)>>1;
+        mergeSort(copyNums,nums,low,mid);//交替使用nums和copyNums
+        mergeSort(copyNums,nums,mid+1,high);
+        mergeTowSubArray(copyNums,nums,low,mid,high);//将最终排序结果拷贝至nums中
+    }
+
+    public void mergeTowSubArray(int[] nums,int[] copyNums,int low,int mid,int high){
+        int i = mid,j = high,indexCopy = high;
+        while(i >= low&&j >= mid+1){
+            copyNums[indexCopy--] = nums[i]>nums[j]?nums[i--]:nums[j--];
+        }
+        while(i>=low) copyNums[indexCopy--] = nums[i--];
+        while(j>=mid+1) copyNums[indexCopy--] = nums[j--];
+    }
 }
