4753058e37f4a25ba274568fb721b9f4418eb128
rainbow2019 <zhaohongzxy@126.com>
2018-08-26 15:39:03 +0800

    提交

---
+++ src/main/java/BinaryTreeBasicAlgorithms/GetDepthOfBinaryTree.java
@@ -0,0 +1,29 @@
+package BinaryTreeBasicAlgorithms;
+
+public class GetDepthOfBinaryTree {
+    /*******************剑指Offer:获取二叉树的深度********************/
+
+    /**
+     * 二叉树的深度定义：从根节点至叶子节点的最长路径的长度
+     *
+     * 思路分析：利用递归方法，递归左子树和右子树，
+     * 分别获取它们深度的最大值再加1即可。
+     */
+    public int getDepth(TreeNode root){
+        if(root == null) return 0;//递归终止条件
+        return Math.max(getDepth(root.left),getDepth(root.right))+1;//求得左子树和右子树的最大值加1即可
+    }
+
+
+    /**
+     * 二叉树的数据结构
+     */
+    public class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        int val;
+        TreeNode(int val){
+            this.val = val;
+        };
+    }
+}
