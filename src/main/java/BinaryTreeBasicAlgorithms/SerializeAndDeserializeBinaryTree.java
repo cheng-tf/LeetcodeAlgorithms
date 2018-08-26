fd8d9796f27e98d62f84db253fbcb1435f4dd2cb
rainbow2019 <zhaohongzxy@126.com>
2018-08-26 15:32:34 +0800

    提交

--- src/main/java/BinaryTreeBasicAlgorithms/SerializeAndDeserializeBinaryTree.java
+++ src/main/java/BinaryTreeBasicAlgorithms/SerializeAndDeserializeBinaryTree.java
@@ -7,15 +7,16 @@ import java.util.Deque;
 
 public class SerializeAndDeserializeBinaryTree {
 
+    /**
+     * 二叉树的数据结构
+     */
     public class TreeNode {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
-
         public TreeNode(int val) {
             this.val = val;
         }
-
     }
 
     @Test
