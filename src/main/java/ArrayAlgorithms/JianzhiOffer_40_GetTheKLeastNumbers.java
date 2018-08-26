f4fd89e6f0bde53689abb961d8b3345db1e0a012
rainbow2019 <zhaohongzxy@126.com>
2018-08-26 17:48:40 +0800

    提交

---
+++ src/main/java/ArrayAlgorithms/JianzhiOffer_40_GetTheKLeastNumbers.java
@@ -0,0 +1,191 @@
+package ArrayAlgorithms;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class JianzhiOffer_40_GetTheKLeastNumbers {
+
+    /***************剑指Offer40:最小的k个数******************/
+
+    /**
+     * 题目描述
+     * 输入n个整数，找出其中最小的K个数。
+     * 例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
+     *
+     * 思路分析:注意边界情况：k小于等于0时或大于数组长度时，直接返回空list；
+     *
+     * 方法1：利用快排对数组排序，然后输出最小的k个数至list中即可；
+     * 方法2：利用partition方法，只要找到分割的mid为k或k-1即可；(剑指Offer超时)
+     *        按理来说，这种算法应该比快排更快，但是超时不通过。
+     * 方法2：待定。
+     */
+
+    /**
+     * 方法1：快排
+     */
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] nums, int k) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(nums == null|| nums.length==0||k<=0||k>nums.length) return list;
+        quickSort(nums,0,nums.length-1);
+        int i = 0;
+        while(i < k ) list.add(nums[i++]);
+        return list;
+    }
+    /**
+     * 快排
+     */
+    public void quickSort(int[] nums,int low,int high){
+        if(low >= high) return;
+        int mid = partition(nums,low,high);
+        quickSort(nums,low,mid-1);
+        quickSort(nums,mid+1,high);
+    }
+    /**
+     * 快排的partition方法
+     */
+    public int partition(int[] nums,int low,int high){
+        int pivot = nums[high];
+        while(low < high){
+            while(low < high && nums[low] <= pivot) low++;
+            if(low < high) nums[high--] = nums[low];
+            while(low < high && nums[high] >= pivot) high--;
+            if(low < high) nums[low++] = nums[high];
+        }
+        nums[high] = pivot;
+        return high;
+    }
+
+    /**
+     * 方法2：仅仅利用partition方法
+     */
+
+    public ArrayList<Integer> GetLeastNumbers_Solution2(int [] nums, int k) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        if(nums == null|| nums.length==0||k < 1||k>nums.length) return list;
+        int low = 0,high = nums.length-1;
+        int mid = partition(nums,low,high);
+        while(mid != k){
+            if(mid > k){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+            mid = partition(nums,low,high);
+        }
+        while(--k >= 0 ) list.add(nums[k]);
+        return list;
+    }
+
+    /**
+     * 方法3：堆排序：
+     * 用最大堆保存这k个数，每次只和堆顶比，如果比堆顶小，删除堆顶，新数入堆。
+     */
+
+    /*
+    import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.Comparator;
+public class Solution {
+   public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+       ArrayList<Integer> result = new ArrayList<Integer>();
+       int length = input.length;
+       if(k > length || k == 0){
+           return result;
+       }
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(k, new Comparator<Integer>() {
+
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2.compareTo(o1);
+            }
+        });
+        for (int i = 0; i < length; i++) {
+            if (maxHeap.size() != k) {
+                maxHeap.offer(input[i]);
+            } else if (maxHeap.peek() > input[i]) {
+                Integer temp = maxHeap.poll();
+                temp = null;
+                maxHeap.offer(input[i]);
+            }
+        }
+        for (Integer integer : maxHeap) {
+            result.add(integer);
+        }
+        return result;
+    }
+}
+     */
+
+    /*
+
+
+
+*基于堆排序算法，构建最大堆。时间复杂度为O(nlogk)
+*如果用快速排序，时间复杂度为O(nlogn)；
+*如果用冒泡排序，时间复杂度为O(n*k)
+
+import java.util.ArrayList;
+    public class Solution {
+        public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+            ArrayList<Integer> list=new ArrayList<Integer>();
+            //检查输入的特殊情况
+            if(input==null || input.length<=0 || input.length<k){
+                return list;
+            }
+            //构建最大堆
+            for(int len=k/2-1; len>=0; len--){
+                adjustMaxHeapSort(input,len,k-1);
+            }
+            //从第k个元素开始分别与最大堆的最大值做比较，如果比最大值小，则替换并调整堆。
+            //最终堆里的就是最小的K个数。
+            int tmp;
+            for(int i=k; i<input.length; i++){
+                if(input[i]<input[0]){
+                    tmp=input[0];
+                    input[0]=input[i];
+                    input[i]=tmp;
+                    adjustMaxHeapSort(input,0,k-1);
+                }
+            }
+            for(int j=0; j<k; j++){
+                list.add(input[j]);
+            }
+            return list;
+        }
+
+        public void adjustMaxHeapSort(int[] input, int pos, int length){
+            int temp;
+            int child;
+            for(temp=input[pos]; 2*pos+1<=length; pos=child){
+                child=2*pos+1;
+                if(child<length && input[child]<input[child+1]){
+                    child++;
+                }
+                if(input[child]>temp){
+                    input[pos]=input[child];
+                }else{
+                    break;
+                }
+            }
+            input[pos]=temp;
+        }
+    }
+
+     */
+
+
+
+    //测试
+    @Test
+    public void test(){
+        int[] nums = {2,3,4,5,1,3,64,23,-3};
+        int[] nums2 = {4,5,1,6,2,7,3,8};
+        ArrayList<Integer> list = GetLeastNumbers_Solution(nums2,17);
+        System.out.println(list.toString());
+        ArrayList<Integer> list2 = GetLeastNumbers_Solution2(nums2,4);
+        System.out.println(list2.toString());
+    }
+
+}
---
+++ src/main/java/BinaryTreeBasicAlgorithms/VerifySquenceOfBSTPreOrderOrPostOrder.java
@@ -0,0 +1,91 @@
+package BinaryTreeBasicAlgorithms;
+
+import org.junit.Test;
+
+public class VerifySquenceOfBSTPreOrderOrPostOrder {
+
+    /*************剑指Offer33：验证数组是否是某二叉搜索树的前序遍历或后序遍历序列****************/
+    /**
+     * 题目介绍：
+     * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+     * 如果是则输出Yes,否则输出No。假设输入的数组的任意两个数字都互不相同。
+     * 
+     * 思路分析：根据二叉搜索树的特点，根节点的左子树的值都小于等于根节点；
+     *           根节点的右子树的值都大于根节点。
+     *           一个二叉搜索树的后序遍历序列的最后一个元素为根节点的值，那么根节点之前
+     *           的可以分成两部分，第一部分都小于根节点的值，第二部分都大于根节点的值。
+     *           然后对第一部分和第二部分再进行递归调用即可。
+     */
+    public boolean VerifySquenceOfBSTPostOrder(int[] sequence) {
+        if(sequence==null||sequence.length==0) return false;
+        return VerifySquenceOfBSTPostOrder(sequence,0,sequence.length-1);
+    }
+
+    /**
+     * 递归调用方法
+     */
+    private boolean VerifySquenceOfBSTPostOrder(int[] sequence,int start,int end){
+        if(end - start < 2) return true;//1个或2个元素一定满足二叉搜索树；3个就不一定了
+        int rootValue = sequence[end];
+        int leftIndex = start;
+        while(leftIndex < end && sequence[leftIndex]<= rootValue) leftIndex++;
+        int rightIndex = leftIndex;
+        while(sequence[rightIndex] > rootValue) rightIndex++;//由于最后一个元素肯定等于rootValue，所以不担心数组索引越界
+        if(rightIndex != end) return false;
+        boolean left = VerifySquenceOfBSTPostOrder(sequence,start,leftIndex-1);
+        boolean right = VerifySquenceOfBSTPostOrder(sequence,leftIndex,end-1);
+        return left&&right;
+    }
+
+    /*****************************举一反三：验证数组是否是某二叉搜索树的前序遍历**************/
+
+    /**
+     * 分析：前序遍历和后序遍历的区别就是：后序遍历是最后一个元素为根节点，
+     * 而前序遍历的第一个元素为根节点，其他的都是相同的。
+     *
+     */
+
+    public boolean VerifySquenceOfBSTPreOrder(int[] sequence) {
+        if(sequence==null||sequence.length==0) return false;
+        return VerifySquenceOfBSTPreOrder(sequence,0,sequence.length-1);
+    }
+
+    /**
+     * 递归调用方法
+     */
+    private boolean VerifySquenceOfBSTPreOrder(int[] sequence,int start,int end){
+        if(end - start < 2) return true;//1个或2个元素一定满足二叉搜索树；3个就不一定了
+        int rootValue = sequence[start];
+        int leftIndex = start+1;
+        while(leftIndex <= end && sequence[leftIndex]<= rootValue) leftIndex++;
+        int rightIndex = leftIndex;
+        while(rightIndex <= end&&sequence[rightIndex] > rootValue) rightIndex++;//由于rootValue在头部，尾部一定要控制数组越界问题
+        if(rightIndex != end+1) return false;
+        boolean left = VerifySquenceOfBSTPostOrder(sequence,start+1,leftIndex-1);
+        boolean right = VerifySquenceOfBSTPostOrder(sequence,leftIndex,end);
+        return left&&right;
+    }
+
+
+
+    //测试
+    @Test
+    public void test(){
+        //测试BST后序遍历
+        int[] sequence = {5,7,6,9,11,10,8};
+        boolean result = VerifySquenceOfBSTPostOrder(sequence);
+        System.out.println("result = " + result);
+        int[] sequence2 = {7,4,6,5};
+        boolean result2 = VerifySquenceOfBSTPostOrder(sequence2);
+        System.out.println("result2 = " + result2);
+
+        //测试BST前序遍历
+        int[] sequence11 = {8,6,5,7,10,9,11};
+        boolean result11 = VerifySquenceOfBSTPreOrder(sequence11);
+        System.out.println("result11 = " + result11);
+        int[] sequence22 = {7,9,6};
+        boolean result22 = VerifySquenceOfBSTPreOrder(sequence22);
+        System.out.println("result22 = " + result22);
+    }
+
+}
