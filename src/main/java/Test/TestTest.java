b3dba7164b32408a66a38614cf9efc810086774e
Yuanyuan <38313097+rainbow2019@users.noreply.github.com>
2018-10-01 16:05:37 +0800

    Revert "222"
    This reverts commit cdaa46b930ba33f57a04306872629b3516e72861.

--- src/main/java/Test/TestTest.java
+++ src/main/java/Test/TestTest.java
@@ -7,7 +7,6 @@ public class TestTest {
     public int NumberOf1(int n) {
         int i = 1;
         int count = 0;
-        method1();
         while(i != 0){
             if((n & i) != 0){
                 count++;
@@ -19,22 +18,7 @@ public class TestTest {
     
     @Test
     public void test(){
-        for (int i = 0;i<10;i++){
-            NumberOf1(i);
-            System.out.println("i = " + i);
-        }
-
-
-        NumberOf1(11);
-        NumberOf1(11);
-        NumberOf1(11);
         System.out.println("NumberOf1(10) = " + NumberOf1(10));
-
-    }
-
-    public void method1(){
-        System.out.println(" = " );
-        return ;
     }
 
 }
--- src/main/java/ThreadValves/ReentrantLockDemo.java
+++ src/main/java/ThreadValves/ReentrantLockDemo.java
@@ -1,16 +1,4 @@
 package ThreadValves;
 
-import java.util.Arrays;
-
 public class ReentrantLockDemo {
-    public static void main(String[] args) {
-        int[] a = {1,2};
-        Arrays.sort(a);
-        Arrays.sort(new int[]{10,11,13});
-        aaa(a);
-    }
-
-    private static void aaa(int[] a) {
-        aaa(a);
-    }
 }
