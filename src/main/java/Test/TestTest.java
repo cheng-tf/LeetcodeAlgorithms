cdaa46b930ba33f57a04306872629b3516e72861
Yuanyuan <38313097+rainbow2019@users.noreply.github.com>
2018-10-01 15:37:44 +0800

    222

--- src/main/java/Test/TestTest.java
+++ src/main/java/Test/TestTest.java
@@ -7,6 +7,7 @@ public class TestTest {
     public int NumberOf1(int n) {
         int i = 1;
         int count = 0;
+        method1();
         while(i != 0){
             if((n & i) != 0){
                 count++;
@@ -18,7 +19,22 @@ public class TestTest {
     
     @Test
     public void test(){
+        for (int i = 0;i<10;i++){
+            NumberOf1(i);
+            System.out.println("i = " + i);
+        }
+
+
+        NumberOf1(11);
+        NumberOf1(11);
+        NumberOf1(11);
         System.out.println("NumberOf1(10) = " + NumberOf1(10));
+
+    }
+
+    public void method1(){
+        System.out.println(" = " );
+        return ;
     }
 
 }
