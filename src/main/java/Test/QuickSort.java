ff5b4330f03d58f517639ab130e2a6a927a6ec78
Yuanyuan <38313097+rainbow2019@users.noreply.github.com>
2018-12-10 17:53:20 +0800

    222

--- src/main/java/Test/QuickSort.java
+++ src/main/java/Test/QuickSort.java
@@ -4,23 +4,22 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-public class QuickSort
-{
+public class QuickSort {
 
     @Test
-    public void test(){
-        int[] nums = {23,45,23,66,34,9,65,3,-34,233,65,232,3,0};
+    public void test() {
+        int[] nums = {23, 45, 23, 66, 34, 9, 65, 3, -34, 233, 65, 232, 3, 0};
         System.out.println(Arrays.toString(nums));
-        quickSort(nums,0,nums.length-1);
+        quickSort(nums, 0, nums.length - 1);
         System.out.println(Arrays.toString(nums));
-        System.out.println(binarySearch(nums,0,nums.length-1,233));
+        System.out.println(binarySearch(nums, 0, nums.length - 1, 233));
 
     }
 
-    public int binarySearch(int[] nums,int start,int end,int target){
-        while(start <= end) {
-            int mid = (start+end)>>1;
-            if (target> nums[mid]) {
+    public int binarySearch(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = (start + end) >> 1;
+            if (target > nums[mid]) {
                 start = mid + 1;
             } else if (target < nums[mid]) {
                 end = mid - 1;
@@ -32,20 +31,20 @@ public class QuickSort
     }
 
 
-    public void quickSort(int[] nums,int start,int end){
-        if(start >= end) return;
-        int mid = partition(nums,start,end);
-        quickSort(nums,start,mid-1);
-        quickSort(nums,mid+1,end);
+    public void quickSort(int[] nums, int start, int end) {
+        if (start >= end) return;
+        int mid = partition(nums, start, end);
+        quickSort(nums, start, mid - 1);
+        quickSort(nums, mid + 1, end);
     }
 
-    public int partition(int[] nums,int start,int end){
+    public int partition(int[] nums, int start, int end) {
         int pivot = nums[end];
-        while(start < end){
-            while(start < end && nums[start]<=pivot) start++;
-            if(start < end) nums[end--] = nums[start];
-            while(start <end &&nums[end]>=pivot) end--;
-            if(start < end) nums[start++] = nums[end];
+        while (start < end) {
+            while (start < end && nums[start] <= pivot) start++;
+            if (start < end) nums[end--] = nums[start];
+            while (start < end && nums[end] >= pivot) end--;
+            if (start < end) nums[start++] = nums[end];
         }
         nums[start] = pivot;
         return start;
