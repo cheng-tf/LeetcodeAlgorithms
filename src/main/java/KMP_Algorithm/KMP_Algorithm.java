9eac574af9b201b070ee7979e087b8507e480a45
Yuanyuan <38313097+rainbow2019@users.noreply.github.com>
2018-12-25 22:05:39 +0800

    KMP算法

--- src/main/java/KMP_Algorithm/KMP_Algorithm.java
+++ src/main/java/KMP_Algorithm/KMP_Algorithm.java
@@ -48,7 +48,6 @@ public class KMP_Algorithm {
      * 当遇到字符不相等的时候，就需要将匹配字符串match右移动，最坏情况下，需要匹配N次，
      * 因此时间复杂度为O(N);
      */
-
     public int getFirstIndexOf(String str, String match) {
         if (str == null || match == null || str.length() < 1 || str.length() < match.length()) return -1;
         char[] strChars = str.toCharArray();
