49831b1a57008412d396e429bdb8a6ac2e8d3824
rainbow2019 <zhaohongzxy@163.com>
2019-03-26 16:09:48 +0800

    KMP算法

--- src/main/java/BinaryTreeBasicAlgorithms/GetDepthOfBinaryTree.java
+++ src/main/java/BinaryTreeBasicAlgorithms/GetDepthOfBinaryTree.java
@@ -1,29 +1,112 @@
 package BinaryTreeBasicAlgorithms;
 
+import org.junit.Test;
+
+import java.util.ArrayDeque;
+
 public class GetDepthOfBinaryTree {
     /*******************剑指Offer:获取二叉树的深度********************/
 
     /**
-     * 二叉树的深度定义：从根节点至叶子节点的最长路径的长度
-     *
-     * 思路分析：利用递归方法，递归左子树和右子树，
-     * 分别获取它们深度的最大值再加1即可。
+     * 二叉树的深度定义：从根节点至叶子节点的最长路径的长度。
+     * <p>
+     * 思路分析：
+     * 两种算法：①递归-深度优先搜索DFS；②循环-广度优先搜索BFS。
+     *    第一种:递归;本质就是：深度优先搜索DFS;
+     *       利用递归方法，递归左子树和右子树，分别获取它们深度的最大值再加1即可。
+     *    第二种：循环-广度优先搜索BFS。
+     *       利用队列数据结构，分别将一层一层的节点添加到队列中，然后利用sum记录一层的数量，
+     *       遍历这一层节点，同时添加它们的子节点到队列中，当sum为0时，更新sum值，同时depth++，开始下一层的遍历。
+     *       直到队列为空时，退出循环。
      */
-    public int getDepth(TreeNode root){
-        if(root == null) return 0;//递归终止条件
-        return Math.max(getDepth(root.left),getDepth(root.right))+1;//求得左子树和右子树的最大值加1即可
+
+    /**
+     * 递归:深度优先搜索DFS
+     */
+    public int getTreeDepthDFS(TreeNode root) {
+        if (root == null) return 0;//递归终止条件
+        return Math.max(getTreeDepthDFS(root.left), getTreeDepthDFS(root.right)) + 1;
+//        int leftDepth = getTreeDepthDFS(root.left);
+//        int rightDepth = getTreeDepthDFS(root.right);
+//        return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;//求得左子树和右子树的最大值加1即可
     }
 
 
     /**
+     * 循环-广度优先搜索
+     */
+    public int getTreeDepthBFS1(TreeNode root) {
+        if (root == null) return 0;
+        ArrayDeque<TreeNode> deque = new ArrayDeque<>();
+        deque.addLast(root);
+        int nextSum = 1, currCount = 0;//nextSum,进入到队列中的元素个数;currCount遍历队列中元素计数
+        int depth = 0;
+        while (!deque.isEmpty()) {
+            TreeNode node = deque.pollFirst();
+            currCount++;
+            if (node.left != null) deque.addLast(node.left);
+            if (node.right != null) deque.addLast(node.right);
+            if (currCount == nextSum) {//当前层遍历结束
+                nextSum = deque.size();//下一层的元素个数
+                currCount = 0;//置位0
+                depth++;//深度加1
+            }
+        }
+        return depth;
+    }
+
+    /**
+     * 循环-广度优先搜索
+     */
+    public int getTreeDepthBFS2(TreeNode root) {
+        if (root == null) return 0;
+        ArrayDeque<TreeNode> deque = new ArrayDeque<>();
+        deque.addLast(root);//将第一层加入队列中
+        int sum = deque.size();//加入队列的一层的节点数
+        int depth = 0;
+        TreeNode node;
+        while (!deque.isEmpty()) {
+            //删除当前层的节点，并将下层节点加入到队列中
+            while (sum-- > 0) {
+                node = deque.pollFirst();
+                if (node.left != null) deque.addLast(node.left);
+                if (node.right != null) deque.addLast(node.right);
+            }
+            sum = deque.size();
+            depth++;
+        }
+        return depth;
+    }
+
+    /**
      * 二叉树的数据结构
      */
-    private class TreeNode{
+    private class TreeNode {
         TreeNode left;
         TreeNode right;
         int val;
-        TreeNode(int val){
+
+        TreeNode(int val) {
             this.val = val;
-        };
+        }
+    }
+
+    /*****************************测试**********************/
+    @Test
+    public void test() {
+        TreeNode root = new TreeNode(0);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node3.left = node4;
+        node4.left = node5;
+        System.out.println(getTreeDepthDFS(root));
+        System.out.println(getTreeDepthBFS1(root));
+        System.out.println(getTreeDepthBFS2(root));
     }
 }
--- src/main/java/BinaryTreeBasicAlgorithms/IsBalancedBinaryTree.java
+++ src/main/java/BinaryTreeBasicAlgorithms/IsBalancedBinaryTree.java
@@ -6,71 +6,82 @@ package BinaryTreeBasicAlgorithms;
  * 递归思想
  */
 public class IsBalancedBinaryTree {
-
-
     /**
-     * 二叉树数据结构
+     * 题目描述
+     * 输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+     * 解题思路：
+     * 算法1：先序遍历的顺序。存在节点多次访问的问题，效率低下。
+     * 算法2：后序遍历的顺序。先判断左右子树是否平衡性，并计算根节点的深度。
+     *       需要利用数组作为参数保存根节点的深度。
      */
-    private  class TreeNode{
-        int val;
-        TreeNode left;
-        TreeNode right;
-        public TreeNode(int val){
-            this.val = val;
-        }
-    }
-
 
     /**
      * 算法1：简单。
-     * 通过分别求解每个节点的左子树和右子树的深度，比较判断。
+     * 通过分别求解每个节点的左子树和右子树的深度。
+     * 只有左右子树的深度差值小于等于1,且左右子树都是平衡树的时候，才能说明当前树是平衡树。
      * 问题：存在一个节点被重复遍历多次的问题，时间效率不高。
      */
-    public boolean isBalancedBinaryTreeByDepth(TreeNode root){
-        if(root == null) return true;//递归的终止条件
+    public boolean isBalancedBinaryTreeByDepth(TreeNode root) {
+        if (root == null) return true;//递归的终止条件
         //分别获取左子树和右子树的深度
-        if(Math.abs(getDepth(root.left)-getDepth(root.right)) > 1){
-            return false;
-        }else {
-            return isBalancedBinaryTreeByDepth(root.left) && isBalancedBinaryTreeByDepth(root.right);
-        }
+//        if (Math.abs(getDepth(root.left) - getDepth(root.right)) > 1) {
+//            return false;
+//        } else {
+//            return isBalancedBinaryTreeByDepth(root.left) && isBalancedBinaryTreeByDepth(root.right);
+//        }
+        return (Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1)
+                && isBalancedBinaryTreeByDepth(root.left)
+                && isBalancedBinaryTreeByDepth(root.right);
     }
 
     /**
      * 根据二叉树的根节点，获取该二叉树的深度(路径的最大长度）
      */
-    public int getDepth(TreeNode root){
-        if(root == null) return 0;
-        return Math.max(getDepth(root.left),getDepth(root.right))+1;
+    public int getDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
     }
+
+
 /**************************************************************************/
     /**
-     * 算法2：从最底层子树判断开始，直到整个二叉树，避免了一个节点被多次遍历的情况
+     * 算法2：从最底层子树判断开始，直到整个二叉树，避免了一个节点被多次遍历的情况.
+     * 具体实现：采用后序遍历的方式，从底层开始判断二叉树的平衡性，避免了节点多次访问的情况。
      */
-    public boolean isBalancedBinaryTree(TreeNode root){
+    public boolean isBalancedBinaryTree(TreeNode root) {
         int[] depth = new int[]{0};//初始值为0,用于存储当前子树的深度
-        return helper(root,depth);//其实这里的depth就是整个二叉树的深度
+        return helper(root, depth);//其实这里的depth就是整个二叉树的深度
     }
-    /**
-     *
-     */
-    public boolean helper(TreeNode root, int[] depth){
-        if(root == null){
+
+    public boolean helper(TreeNode root, int[] depth) {
+        if (root == null) {
             depth[0] = 0;//空，深度为0
             return true;
         }
         int[] leftDepth = new int[]{0};//用于存储当前节点的左子树的深度
         int[] rightDepth = new int[]{0};//用于存储当前节点的右子树的深度
-        if(helper(root.left,leftDepth)&&helper(root.right,rightDepth)){
-            if(leftDepth[0]-rightDepth[0]<=1&&leftDepth[0]-rightDepth[0]>=-1){
-                //返回true之前，保存当前节点为根节点的二叉树的深度，递归的上一层函数需要
-//                depth[0] = Math.max(leftDepth[0],rightDepth[0])+1;
-                depth[0] = leftDepth[0]>rightDepth[0]?leftDepth[0]+1:rightDepth[0]+1;//避免使用Math
-                return true;
-            }
+        //左子树和右子树都是平衡树，且两者的深度差值不能超过1,返回true
+        if (helper(root.left, leftDepth)
+                && helper(root.right, rightDepth)
+                && Math.abs(leftDepth[0] - rightDepth[0]) <= 1) {
+            //返回true之前，保存当前节点为根节点的二叉树的深度，递归的上一层函数需要
+            depth[0] = Math.max(leftDepth[0], rightDepth[0]) + 1;
+            return true;
         }
         return false;
     }
 
+    /**
+     * 二叉树数据结构
+     */
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
 
 }
